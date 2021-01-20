d47c5cc3d877069b39a7a8de1d8b2dad87bb2fbf
wuyulong <1679187550@qq.com>
2021-01-20 21:54:52 +0800

    setTimeout实现红绿灯效果

--- js/traffic.js
+++ js/traffic.js
@@ -1,3 +1,28 @@
-var green = document.getElementsByClassName('green')[0]
-green.style.backgroundColor = "#fff"
+var green = document.getElementsByClassName('green')[0],
+    orange = document.getElementsByClassName('orange')[0],
+    red = document.getElementsByClassName('red')[0];
 //写一个宏观任务 让他每6秒执行一次 在宏观任务中使用微观异步任务 让他们分别
+function traffic() {
+  setTimeout(() =>{
+    red.style.backgroundColor = '#000'
+    red.style.boxShadow = '-1px -1px 2px #fff'
+    orange.style.backgroundColor = 'orange'
+    orange.style.boxShadow = 'none'
+    setTimeout(() => {
+      orange.style.backgroundColor = '#000'
+      orange.style.boxShadow = '-1px -1px 2px #fff'
+      green.style.backgroundColor = 'green'
+      green.style.boxShadow = 'none'
+      setTimeout(() => {
+        green.style.backgroundColor = '#000'
+        green.style.boxShadow = '1px -1px 2px #fff'
+        red.style.backgroundColor = 'red'
+        red.style.boxShadow = 'none'
+        setTimeout(traffic(), 0)
+      }, 3000)
+    }, 1000)
+  }, 2000)
+}
+traffic()
+
+
